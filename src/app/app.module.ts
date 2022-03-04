f2c99b0473c7509851df1380bd139c70f9c6f08d
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-03-04 07:26:01 +0530

    Create cardForm Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardFormComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
